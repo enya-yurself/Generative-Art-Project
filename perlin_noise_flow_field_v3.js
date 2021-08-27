bd4271a5fab9593433d986cad2863ceba8ac0838
smileymittens <61215159+smileymittens@users.noreply.github.com>
2021-08-27 17:50:38 -0400

    renamed, added color options

--- perlin_noise_flow_field_v3.js
+++ perlin_noise_flow_field_v3.js
@@ -5,29 +5,47 @@ var zoff = 0;
 var fr;
 var particles = [];
 var flowfield;
-var numparts=400; //add a slider here
+var numparts = 400; //add a slider here
+let colorPicker;
+let modeSelect;
+let colorInc = 0;
+let lerpDir = true; //true = towards color2, false = towards color1
 
 function setup() {
   createCanvas(400, 400);
-  background(255)
+  colorMode(HSB, 360, 100, 100, 100);
+  background(255);
   cols = floor(width / scl);
   rows = floor(height / scl);
-  fr = createP('')
-  flowfield=new Array(cols*rows)
+  fr = createP('');
+  flowfield = new Array(cols * rows);
   for (var i = 0; i < numparts; i++) {
     particles[i] = new Particle();
   }
+  modeSelect = createSelect();
+  modeSelect.option("Black");
+  modeSelect.option("Rainbow");
+  modeSelect.option("Gradient");
+  color1 = createColorPicker('#ff968f');
+  color1.position(0, 0);
+  color2 = createColorPicker('#37bbbf');
+  color2.position(50, 0);
+}
+
+function clearBG() {
+  background(0, 0, 100)
 }
 
 function draw() {
+  modeSelect.changed(clearBG);
   var yoff = 0;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
       var index = x + y * cols
-      var angle = noise(xoff, yoff, zoff) * TWO_PI*4;
-      var v = p5.Vector.fromAngle(angle)
-      v.setMag(.7) //add a slider here
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(.7); //add a slider here
       flowfield[index] = v;
       xoff += inc
       // stroke(0,30); //add slider for alpha value?
@@ -39,7 +57,7 @@ function draw() {
       // pop();
     }
     yoff += inc;
-    zoff += inc/160; //add a slider
+    zoff += inc / 160; //add a slider
   }
   for (var i = 0; i < particles.length; i++) {
     particles[i].follow(flowfield);
